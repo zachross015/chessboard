53233f34f05d6de4c8c1f8d8b2f421691c97183e
Zachary Ross <zachross015@gmail.com>
2023-07-05 13:16:11 -0400

    All pieces have been added to the board

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,7 @@ fn main() {
                     }
                 ))
                 .add_startup_system(setup)
+                .add_startup_system(load_pieces)
                 .insert_resource(c)
                 .run();
             }
@@ -45,13 +46,57 @@ fn main() {
     }
 }
 
+fn board_position(position: (u8, u8), config: &Res<Config>) -> Vec3 {
+    let xf = ((position.0 as f32) - 4.0) * config.space_size;
+    let yf = ((position.1 as f32) - 4.0) * config.space_size;
+    Vec3::new(xf, yf, 0.0)
+}
+
+#[derive(Component)]
+struct Piece; 
+
+fn load_pieces(
+    mut commands: Commands,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    asset_server: Res<AssetServer>,
+    config: Res<Config>,
+    ) {
+    // Load pieces
+    let texture_handle = asset_server.load("textures/chess_pieces.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(2560. / 6., 853. / 2.), 6, 2, None, None);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    let pieces = vec![
+        0, 1, 2, 2, 3, 3, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5,
+        6, 7, 8, 8, 9, 9, 10, 10, 11, 11, 11, 11, 11, 11, 11, 11
+    ];
+    let positions = vec![
+        (3, 0), (4, 0), (2, 0), (5, 0), (1, 0), (6, 0), (0, 0), (7, 0),
+        (3, 1), (4, 1), (2, 1), (5, 1), (1, 1), (6, 1), (0, 1), (7, 1),
+        (3, 7), (4, 7), (2, 7), (5, 7), (1, 7), (6, 7), (0, 7), (7, 7),
+        (3, 6), (4, 6), (2, 6), (5, 6), (1, 6), (6, 6), (0, 6), (7, 6),
+    ];
+    
+    for (piece, position) in pieces.iter().zip(positions.iter()) {
+        commands.spawn((SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle.clone(),
+            sprite: TextureAtlasSprite {
+                index: *piece,
+                custom_size: Some(Vec2::new(config.space_size, config.space_size)),
+                anchor: Anchor::BottomLeft,
+                ..default()
+            },
+            transform: Transform::from_translation(board_position(*position, &config)),
+            ..default()
+        }, Piece));
+    }
+}
+
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
     config: Res<Config>,
-    asset_server: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
     ) {
 
     commands.spawn(Camera2dBundle::default());
@@ -74,18 +119,4 @@ fn setup(
         }
     }
 
-    // Load pieces
-    let texture_handle = asset_server.load("textures/chess_pieces.png");
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(2560. / 6., 853. / 2.), 6, 2, None, None);
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
-    commands.spawn(SpriteSheetBundle {
-        texture_atlas: texture_atlas_handle.clone(),
-        sprite: TextureAtlasSprite {
-            index: 0,
-            custom_size: Some(Vec2::new(config.space_size, config.space_size)),
-            anchor: Anchor::BottomLeft,
-            ..default()
-        },
-        ..default()
-    });
 }
